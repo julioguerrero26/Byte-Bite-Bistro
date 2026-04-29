03c02eeedbb9829e7c6f5e117429be4dd8d797c0
Juju Guerrero <juliocesarng25@gmail.com>
2026-04-29 17:40:49 -0600

    si compilaaaaa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
@@ -20,6 +19,8 @@ export default defineConfig({
 					href: 'https://github.com/julioguerrero26'
 				},
 			],
+			sidebar: [],
+			tableOfContents: false
 		}),
 	],
 });
