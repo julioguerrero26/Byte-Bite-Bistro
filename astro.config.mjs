879b1767f37a23bfa07f69c4a7f431abb917628a
Juju Guerrero <juliocesarng25@gmail.com>
2026-04-29 14:50:22 -0600

    si compila

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,23 +2,22 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
+			title: 'Menu',
+			logo: {
+				src: './src/assets/leon.svg',
+				replacesTitle: true,
+			},
+			customCss: [
+				'./src/Styles/custom.css',
+			],
+			social: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					icon: 'github',
+					label: 'GitHub',
+					href: 'https://github.com/julioguerrero26'
 				},
 			],
 		}),
